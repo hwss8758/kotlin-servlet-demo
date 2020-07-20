43833d2417b9e9633269f3201fcf440ca5d5ae04
wonsang.hwang <wonsang.hwang@navercorp.com>
2020-07-20 11:22:34 +0900

    Web application context에만 bean 설정
    root WebApplicationContext는 삭제하도록 처리

--- src/main/java/me/wonsang/WebConfig.kt
+++ src/main/java/me/wonsang/WebConfig.kt
@@ -2,9 +2,8 @@ package me.wonsang
 
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
-import org.springframework.stereotype.Controller
 
 @Configuration
-@ComponentScan(useDefaultFilters = false, includeFilters = [ComponentScan.Filter(Controller::class)])
+@ComponentScan
 open class WebConfig {
 }
